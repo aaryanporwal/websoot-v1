2686bac6a92e8f27a98d7bbc8381427017f5de09
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-01-26 02:47:34 +0530

    UX optimizations and added hamburger icon

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,45 @@ export default function Home() {
 
       <header className="py-6">
         <div className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
-          <div className="text-4xl wei sm:text-5xl font-extrabold">
-            <span className="animated-gradient">Aaryan Porwal</span>
+          <div className="text-4xl sm:text-5xl font-extrabold">
+            <span className="animated-gradient tracking-tighter">
+              Aaryan Porwal
+            </span>
           </div>
+
+          {/* nav links start from here: */}
           <div className="links hidden md:flex space-x-12 items-center">
-            <a href="#" className="text-selected-text">
+            <a href="#" className="hover:text-selected-text duration-300">
               Projects
             </a>
-            <a href="#">Scrapbook</a>
-            <a href="#">Blog</a>
-            <a href="#resume">
-              <button className="px-6 py-2 bg-theme font-bold">Resume</button>
+            <a href="#" className="hover:text-selected-text duration-300">
+              Scrapbook
             </a>
+            <a href="#" className="hover:text-selected-text duration-300">
+              Blog
+            </a>
+            <a href="#resume" className="">
+              <button className="rounded-md px-6 py-2 bg-theme font-bold hover:scale-110 ease-in-out duration-300 ">
+                Resume
+              </button>
+            </a>
+          </div>
+
+          <div className="md:hidden">
+            <svg
+              className="stroke-cyan-500 "
+              width="34"
+              height="34"
+              viewBox="0 0 26 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                className="hover:fill-cyan-500 duration-300"
+                d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z"
+                fill="white"
+              />
+            </svg>
           </div>
         </div>
       </header>
