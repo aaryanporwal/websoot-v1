97446f78c0e2715e134550f0094133114ebdc203
Aaryan Porwal <aaryanporwal2233@gmail.com>
2022-01-28 01:29:51 +0530

    Added basic hero section

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,8 +7,8 @@ export default function Header() {
   return (
     <>
       <header className="py-6">
-        <div className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
-          <div className="text-4xl sm:text-5xl font-extrabold">
+        <div className="container flex justify-between items-center mx-auto px-8 md:px-10 lg:px-24 w-full">
+          <div className="text-4xl sm:text-4xl font-extrabold md:text-3xl lg:text-4xl">
             <span className="animated-gradient tracking-tighter">
               Aaryan Porwal
             </span>
@@ -16,17 +16,26 @@ export default function Header() {
 
           {/* nav links start from here: */}
           <div className="links hidden md:flex space-x-12 items-center">
-            <a href="#" className="hover:text-selected-text duration-300">
+            <a
+              href="#"
+              className="hover:text-selected-text hover:scale-125 ease-in-out duration-300"
+            >
               Projects
             </a>
-            <a href="#" className="hover:text-selected-text duration-300">
+            <a
+              href="#"
+              className="hover:text-selected-text hover:scale-125 ease-in-out duration-300"
+            >
               Scrapbook
             </a>
-            <a href="#" className="hover:text-selected-text duration-300">
+            <a
+              href="#"
+              className="hover:text-selected-text duration-300 hover:scale-125 ease-in-out"
+            >
               Blog
             </a>
             <a href="#resume" className="">
-              <button className="rounded-md px-6 py-2 bg-theme font-bold hover:scale-110 ease-in-out duration-300 ">
+              <button className="rounded-md px-6 py-2 bg-theme font-bold hover:scale-125 ease-in-out duration-300 ">
                 Resume
               </button>
             </a>
@@ -40,11 +49,12 @@ export default function Header() {
                 width="40"
                 height="40"
                 viewBox="0 0 26 18"
-                fill="none"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="white"
               >
-                <path d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z" />
+                <path
+                  d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z"
+                />
               </svg>
             </button>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Header from "../components/Header";
-
+import NavBar from "../components/NavBar";
+import Typical from "react-typical";
 
 export default function Home() {
   return (
@@ -10,12 +10,45 @@ export default function Home() {
         <meta name="description" content="Welcome! 😁" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <main>
-        {/* <h1 className="text-3xl font-bold underline text-center">
-          Hello World!
-        </h1> */}
-      </main>
+      <NavBar />
+
+      <div className={"bg-dark-100 px-4 sm:px-8"}>
+        <div
+          className={
+            "mx-auto flex w-full max-w-prose flex-col justify-center py-16"
+          }
+        >
+          <section
+            className={
+              "mt-16 w-full self-start text-dark-900 dark:text-gray-50"
+            }
+          >
+            <h1 className={"text-4xl font-semibold"}>Hi I'm Aaryan 👋.</h1>
+            <br />
+            <p className={"text-2xl font-semibold"}>
+              I'm a pre-final year Computer Science student, passionate
+              developer and I can't stop myself from exploring the unexplored.
+              Ever.
+            </p>
+            <br />
+            <h3 className={"text-2xl"}>
+              Interested in: <br />
+              <Typical
+                steps={[
+                  "Web Development",
+                  1200,
+                  "Node.js",
+                  1200,
+                  "System Administration",
+                  1200,
+                ]}
+                loop={Infinity}
+                wrapper="p"
+              />
+            </h3>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
